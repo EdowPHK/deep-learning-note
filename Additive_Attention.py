8c2c435982b4d3596de052a6f7731ffca27e26a8
EdowPHK <edowphk@gmail.com>
2026-05-02 21:44:18 +0800

    Add Additive Attention

---
+++ Additive_Attention.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+class AdditiveAttention(nn.Module):
+    def __init__(self, key_size, query_size, num_hiddens, dropout, **kwargs):
+        super(AdditiveAttention, self).__init__(**kwargs)
+        self.W_k = nn.Linear(key_size, num_hiddens, bias=False)
+        self.W_q = nn.Linear(query_size, num_hiddens, bias=False)
+        self.w_v = nn.Linear(num_hiddens, 1, bias=False)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, queries, keys, values, valid_lens):
+        queries, keys = self.W_q(queries), self.W_k(keys)
+        features = queries.unsqueeze(2) + keys.unsqueeze(1)
+        features = torch.tanh(features)
+        scores = self.w_v(features).squeeze(-1)
+        self.attention_weights = d2l.masked_softmax(scores, valid_lens)
+        return torch.bmm(self.dropout(self.attention_weights), values)
