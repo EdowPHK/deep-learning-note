333a43ab0641a9b817166c9c4ef7140b6c91c165
EdowPHK <edowphk@gmail.com>
2026-05-14 15:18:15 +0800

    Add gitignore

---
+++ Transformer.py
@@ -0,0 +1,15 @@
+import math
+import pandas as pd
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+class PositionWiseFFN(nn.Module):
+    def __init__(self, ffn_num_input, ffn_num_hiddens, ffn_num_outputs, **kwargs):
+        super(PositionWiseFFN, self).__init__(**kwargs)
+        self.dense1 = nn.Linear(ffn_num_input, ffn_num_hiddens)
+        self.relu = nn.ReLU()
+        self.dense2 = nn.Linear(ffn_num_hiddens, ffn_num_outputs)
+
+    def forward(self, X):
+        return self.dense2(self.relu(self.dense1(X)))
